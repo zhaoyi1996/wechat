1f623b680d5bce78b15f02a837c7c86149a263c8
zhaoyi <1113252039@qq.com>
2020-07-08 16:24:35 +0800

    再次修改首页内容

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    欢迎来到我的地牌。
                 </div>
 
                 <div class="links">
